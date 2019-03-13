e27b798866bbf6cb8d985d613374353300d9529e
MrG1raffe <32130231+MrG1raffe@users.noreply.github.com>
2019-03-13 22:26:57 +0300

    Create mz02-5.cpp

---
+++ contest_2/mz02-5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+class S
+{
+    int res;
+    bool is_exit, is_print;
+public:
+    S(): res(0), is_exit(false), is_print(false)
+    {
+        if (!(std::cin >> res)) {
+            is_exit = true;
+        }
+    }
+    S(S &&x): res(0)
+    {
+        if (!(std::cin >> res)) {
+            is_print = true;
+            is_exit = true;
+        } else {
+            is_print = is_exit = false;
+        }
+        res += x.res;
+    }
+    ~S()
+    {
+        if (is_print) {
+            std::cout << res << std::endl;
+        }
+    }
+    operator bool()
+    {
+        return !is_exit;
+    }
+};
