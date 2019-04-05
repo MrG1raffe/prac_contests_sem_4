931497212398d390d8d267947ed82f706872c26f
MrG1raffe <32130231+MrG1raffe@users.noreply.github.com>
2019-04-05 20:18:15 +0300

    06-4, 07-5

---
+++ contest_6/mz06-4.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <iostream>
+
+namespace Game
+{
+    template <class T>
+    class Coord
+    {
+    public:
+        typedef T value_type;
+        T row, col;
+        Coord(T a = T(), T b = T()): row(a), col(b) {}
+    };
+
+    template <class T>
+    T dist(const Coord<T> &rect_size, const Coord<T> &a, const Coord<T> &b)
+    {
+        T dx = (a.col - b.col > 0) ? (a.col - b.col) : (b.col - a.col);
+        T delta = b.row - a.row;
+        T dy = (delta > 0) ? delta : -delta;
+        bool up = delta < 0, down = delta > 0;
+        T dif;
+        if ((down && !(a.col % 2)) || (up && a.col % 2)) {
+            dif = dx / 2 + dx % 2;
+        } else {
+            dif = dx / 2;
+        }
+        if (dif > dy) {
+            dif = dy;
+        }
+        return dx + dy - dif;
+    }
+}
--- contest_7/mz07-3.cpp
+++ contest_7/mz07-3.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/gregorian/greg_date.hpp>
+#include "boost/date_time/gregorian/gregorian.hpp" 
 #include <string>
 
 int main(void)
@@ -9,8 +11,9 @@ int main(void)
     std::string s1, s2;
     std::cin >> s1;
     while (std::cin >> s2) {
-        date d1 = from_string(s1), d2 = from_string(s2);
-        res += abs((d1 - d2).days())
+        date d1(from_simple_string(s1));
+        date d2(from_simple_string(s2));
+        res += abs((d1 - d2).days());
         s1 = s2;
     }
     std::cout << res << std::endl;
